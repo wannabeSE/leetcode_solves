67b5b9c647c7b004128c30139186539ddcac079d
wannabeSE <samirk724@gmail.com>
2024-05-06 00:04:23 +0600

    solve/ two pointer

---
+++ 167. TwoSum-InputArrayIsSorted/arraySortedTwoSum.dart
@@ -0,0 +1,24 @@
+class Solution {
+  List<int> twoSum(List<int> numbers, int target) {
+    int left = 0;
+    int right = numbers.length - 1;
+    while (left < right) {
+      int sum = numbers[left] + numbers[right];
+      if (sum == target) {
+        return [left + 1, right + 1];
+      } else if (sum < target) {
+        right++;
+      } else {
+        left--;
+      }
+    }
+    return [];
+  }
+}
+
+void main(List<String> args) {
+  Solution s = Solution();
+  var res = s.twoSum([-1, 0], -1);
+  print(res);
+
+}
