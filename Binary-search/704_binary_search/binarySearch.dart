1f1c84b9180314fe21fdcc2dc8434790a6da519b
wannabeSE <samirk724@gmail.com>
2024-06-06 00:11:46 +0600

    solved/ binarySearch

---
+++ Binary-search/704_binary_search/binarySearch.dart
@@ -0,0 +1,22 @@
+class Solution {
+  int search(List<int> nums, int target) {
+    int l = 0;
+    int r = nums.length - 1;
+    while (l <= r) {
+      int mid = l + (r - l) ~/ 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return -1;
+  }
+}
+
+void main(List<String> args) {
+  Solution s = Solution();
+  print(s.search([-1, 0, 3, 5, 9, 12], 9));
+}
